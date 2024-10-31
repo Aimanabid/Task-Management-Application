05a5685c24a957aec6846648d1366ef7230c8dc7
Aiman Abid <147660353+Aimanabid@users.noreply.github.com>
2024-10-31 11:37:58 +0500

    Update Model.js

--- backend/Tasks/Model.js
+++ backend/Tasks/Model.js
@@ -3,6 +3,7 @@ const mongoose = require ('mongoose')
 const taskSchema = new mongoose.Schema({
     title :  String,
     message : String,
+    userId : {type : mongoose.Schema.Types.ObjectId, ref : 'Registration'}
 })
 
 module.exports = mongoose.model('tasks',taskSchema)
